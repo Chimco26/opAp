f5ce4db31786aae8957814460527f0028ff8260e
Oren Othnay <oren.othnay@falcore.co>
2017-05-04 22:30:42 +0300

    1. upload keystore 2. Add Chinese language 3. Add release APK v0.5.2

--- app/src/main/java/com/operatorsapp/utils/ChangeLang.java
+++ app/src/main/java/com/operatorsapp/utils/ChangeLang.java
@@ -2,7 +2,6 @@ package com.operatorsapp.utils;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.text.TextUtils;
