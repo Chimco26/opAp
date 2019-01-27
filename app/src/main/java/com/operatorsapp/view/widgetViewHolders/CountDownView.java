75e986e6f4a593984c0deac4c6649fe235b7c78d
Nathan <nathanb@ravtech.co.il>
2019-01-27 20:06:10 +0200

    update countdown canvas

--- app/src/main/java/com/operatorsapp/view/widgetViewHolders/CountDownView.java
+++ app/src/main/java/com/operatorsapp/view/widgetViewHolders/CountDownView.java
@@ -3,7 +3,9 @@ package com.operatorsapp.view.widgetViewHolders;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.view.View;
@@ -12,10 +14,16 @@ import com.operatorsapp.R;
 
 public class CountDownView extends View {
 
-    private static final int STROKE_WIDTH = 20;
-    private Paint mBasePaint, mDegreesPaint, mCenterPaint, mRectPaint;
+    private static final int STROKE_WIDTH = 5;
+    private Paint mBasePaint, mDegreesPaint, mCenterPaint, mBackgroundPaint,
+            mIndicatorPaint, mIndicatorStrokePaint, mTextPaint;
     private RectF mRect;
     private int centerX, centerY, radius;
+    private float padding;
+    private int percentAngle;
+    private float indicatorRadius;
+    private float textSize;
+    private String text;
 
     public CountDownView(Context context) {
         super(context);
@@ -34,9 +42,15 @@ public class CountDownView extends View {
 
     private void init()
     {
-//        mRectPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-//        mRectPaint.setColor(ContextCompat.getColor(getContext(), R.color.magenta));
-//        mRectPaint.setStyle(Paint.Style.FILL);
+        text = "20:00";
+        float density = getResources().getDisplayMetrics().density;
+        padding = 7 * density;
+        percentAngle = 270;
+        indicatorRadius = 6 * density;
+        textSize = 28 * density;
+        mBackgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBackgroundPaint.setStyle(Paint.Style.FILL);
+        mBackgroundPaint.setColor(ContextCompat.getColor(getContext(), R.color.divider_gray));
 
         mCenterPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mCenterPaint.setColor(ContextCompat.getColor(getContext(), R.color.white));
@@ -45,13 +59,27 @@ public class CountDownView extends View {
         mBasePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mBasePaint.setStyle(Paint.Style.STROKE);
         mBasePaint.setStrokeWidth(STROKE_WIDTH);
-        mBasePaint.setColor(ContextCompat.getColor(getContext(), R.color.blue1));
+        mBasePaint.setColor(ContextCompat.getColor(getContext(), R.color.black));
 
         mDegreesPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mDegreesPaint.setStyle(Paint.Style.STROKE);
         mDegreesPaint.setStrokeWidth(STROKE_WIDTH);
-        mDegreesPaint.setColor(ContextCompat.getColor(getContext(), R.color.red_line));
+        mDegreesPaint.setColor(ContextCompat.getColor(getContext(), R.color.blue1));
 
+        mIndicatorPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mIndicatorPaint.setColor(ContextCompat.getColor(getContext(), R.color.white));
+        mIndicatorPaint.setStyle(Paint.Style.FILL);
+
+        mIndicatorStrokePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mIndicatorStrokePaint.setColor(ContextCompat.getColor(getContext(), R.color.blue1));
+        mIndicatorStrokePaint.setStyle(Paint.Style.STROKE);
+        mIndicatorStrokePaint.setStrokeWidth(STROKE_WIDTH / 2);
+
+        mTextPaint = new Paint();
+        mTextPaint.setColor(ContextCompat.getColor(getContext(), R.color.blue1));
+        mTextPaint.setStyle(Paint.Style.FILL);
+        mTextPaint.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.BOLD));
+        mTextPaint.setTextSize(textSize);
     }
 
 
@@ -62,40 +90,36 @@ public class CountDownView extends View {
         // getHeight() is not reliable, use getMeasuredHeight() on first run:
         // Note: mRect will also be null after a configuration change,
         // so in this case the new measured height and width values will be used:
-        if (mRect == null)
-        {
-            // take the minimum of width and height here to be on he safe side:
+        if (mRect == null) {
             centerX = getMeasuredWidth()/ 2;
             centerY = getMeasuredHeight()/ 2;
             radius = Math.min(centerX,centerY);
 
-            // mRect will define the drawing space for drawArc()
-            // We have to take into account the STROKE_WIDTH with drawArc() as well as drawCircle():
-            // circles as well as arcs are drawn 50% outside of the bounds defined by the radius (radius for arcs is calculated from the rectangle mRect).
-            // So if mRect is too large, the lines will not fit into the View
-            int startTop = STROKE_WIDTH / 2;
-            int startLeft = startTop;
+            float start = (STROKE_WIDTH / 2) + padding;
+            float end = (2 * radius) - start - padding;
 
-            int endBottom = 2 * radius - startTop;
-            int endRight = endBottom;
-
-            mRect = new RectF(startTop, startLeft, endRight, endBottom);
+            mRect = new RectF(start, start, end, end);
         }
 
-        // just to show the rectangle bounds:
-//        canvas.drawRect(mRect, mRectPaint);
+        canvas.drawCircle(centerX, centerY, radius - STROKE_WIDTH / 2, mBackgroundPaint);
+        canvas.drawCircle(centerX, centerY, radius - STROKE_WIDTH - padding, mCenterPaint);
 
-        // subtract half the stroke width from radius so the blue circle fits inside the View
-        canvas.drawCircle(centerX, centerY, radius - STROKE_WIDTH / 2, mBasePaint);
-        // Or draw arc from degree 192 to degree 90 like this ( 258 = (360 - 192) + 90:
-        // canvas.drawArc(mRect, 192, 258, false, mBasePaint);
+        canvas.drawCircle(centerX, centerY, radius - STROKE_WIDTH / 2 - padding, mBasePaint);
 
-        // draw an arc from 90 degrees to 192 degrees (102 = 192 - 90)
-        // Note that these degrees are not like mathematical degrees:
-        // they are mirrored along the y-axis and so incremented clockwise (zero degrees is always on the right hand side of the x-axis)
-        canvas.drawArc(mRect, 90, 102, false, mDegreesPaint);
+        canvas.drawArc(mRect, -90, -percentAngle, false, mDegreesPaint);//-90 because start from 90 degrees after (0 = center top)
+        canvas.drawCircle(getPercentArcEndPoint(percentAngle).x, getPercentArcEndPoint(percentAngle).y, indicatorRadius, mIndicatorPaint);
+        canvas.drawCircle(getPercentArcEndPoint(percentAngle).x, getPercentArcEndPoint(percentAngle).y, indicatorRadius, mIndicatorStrokePaint);
 
-        // subtract stroke width from radius so the white circle does not cover the blue circle/ arc
-        canvas.drawCircle(centerX, centerY, radius - STROKE_WIDTH, mCenterPaint);
+        mTextPaint.setTextAlign(Paint.Align.CENTER);
+        canvas.drawText(text, centerX, centerY + textSize/2, mTextPaint);
+    }
+
+    private Point getPercentArcEndPoint(double angle){
+        angle = -Math.toRadians(angle - 90);//because arc start at 90 degrees and need be in radians for trigonometric functions
+        Point point = new Point();
+        point.x = (int) (centerX + (radius - indicatorRadius - STROKE_WIDTH) * Math.cos(angle));
+        point.y = (int) (centerY + (radius - indicatorRadius - STROKE_WIDTH) * Math.sin(angle));
+
+        return point;
     }
 }
