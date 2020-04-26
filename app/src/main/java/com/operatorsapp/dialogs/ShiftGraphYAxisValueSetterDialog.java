f81e5aa66d8d7a22a3a61db211e1588419379401
nathan <ndb994@gmail.com>
2020-04-26 14:33:52 +0300

    fix suctom Y value axis in shift report dialog

--- app/src/main/java/com/operatorsapp/dialogs/ShiftGraphYAxisValueSetterDialog.java
+++ app/src/main/java/com/operatorsapp/dialogs/ShiftGraphYAxisValueSetterDialog.java
@@ -3,10 +3,10 @@ package com.operatorsapp.dialogs;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -38,13 +38,9 @@ public class ShiftGraphYAxisValueSetterDialog implements View.OnClickListener {
         minEt = view.findViewById(R.id.SGY_min_et);
         maxEt = view.findViewById(R.id.SGY_max_et);
         activateSwitchBtn = view.findViewById(R.id.SGY_activate_switch);
-        if (PersistenceManager.getInstance().getShiftGraphMinY() != -1) {
-            minEt.setText(String.valueOf(PersistenceManager.getInstance().getShiftGraphMinY()));
-        }
-        if (PersistenceManager.getInstance().getShiftGraphMaxY() != -1) {
-            maxEt.setText(String.valueOf(PersistenceManager.getInstance().getShiftGraphMaxY()));
-        }
         activateSwitchBtn.setChecked(PersistenceManager.getInstance().isShiftCustomY());
+        initText(activateSwitchBtn.isChecked(), String.valueOf(PersistenceManager.getInstance().getShiftGraphMinY()),
+                String.valueOf(PersistenceManager.getInstance().getShiftGraphMaxY()));
         TextView positiveBtn = view.findViewById(R.id.DALJ_positive_btn);
 
         builder.setCancelable(true);
@@ -59,7 +55,44 @@ public class ShiftGraphYAxisValueSetterDialog implements View.OnClickListener {
             }
         });
 
+        activateSwitchBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b && minEt.getText().length() == 0 && maxEt.getText().length() == 0) {
+                    initText(b, minEt.getHint().toString(), maxEt.getHint().toString());
+                } else if (b && minEt.getText().length() == 0 && maxEt.getText().toString().length() > 0) {
+                    initText(b, minEt.getHint().toString(), maxEt.getText().toString());
+                } else if (b && minEt.getText().length() > 0 && maxEt.getText().toString().length() == 0) {
+                    initText(b, minEt.getText().toString(), maxEt.getHint().toString());
+                } else {
+                    initText(b, minEt.getText().toString(), maxEt.getText().toString());
+                }
+            }
+        });
+
         return mAlarmAlertDialog;
+    }
+
+    private void initText(boolean b, String min, String max) {
+        if (b) {
+            if (!(min.equals("-1") && max.equals("-1"))) {
+                minEt.setText(min);
+                minEt.setHint(null);
+                minEt.setSelection(minEt.getText().toString().length());
+
+                maxEt.setText(String.valueOf(max));
+                maxEt.setHint(null);
+                maxEt.setSelection(maxEt.getText().toString().length());
+            }
+        } else {
+            if (!(min.equals("-1") && max.equals("-1"))) {
+                minEt.setText(null);
+                minEt.setHint(min);
+
+                maxEt.setText(null);
+                maxEt.setHint(String.valueOf(max));
+            }
+        }
     }
 
     @Override
@@ -70,20 +103,25 @@ public class ShiftGraphYAxisValueSetterDialog implements View.OnClickListener {
                 mAlarmAlertDialog.dismiss();
                 break;
             case R.id.DALJ_positive_btn:
-                try {
-                    float min = Float.parseFloat(minEt.getText().toString());
-                    float max = Float.parseFloat(maxEt.getText().toString());
-                    if (max > min) {
-                        PersistenceManager.getInstance().setShiftCustomY(activateSwitchBtn.isChecked());
-                        PersistenceManager.getInstance().setShiftGraphMinY(min);
-                        PersistenceManager.getInstance().setShiftGraphMaxY(max);
-                        mListener.onSaveYAxisValues();
-                        mAlarmAlertDialog.dismiss();
-                    }else {
-                        Toast.makeText(v.getContext(), v.getContext().getText(R.string.max_value_must_be_greater_than_min), Toast.LENGTH_SHORT).show();
+                PersistenceManager.getInstance().setShiftCustomY(activateSwitchBtn.isChecked());
+                if (activateSwitchBtn.isChecked()) {
+                    try {
+                        float min = Float.parseFloat(minEt.getText().toString());
+                        float max = Float.parseFloat(maxEt.getText().toString());
+                        if (max > min) {
+                            PersistenceManager.getInstance().setShiftGraphMinY(min);
+                            PersistenceManager.getInstance().setShiftGraphMaxY(max);
+                            mListener.onSaveYAxisValues();
+                            mAlarmAlertDialog.dismiss();
+                        } else {
+                            Toast.makeText(v.getContext(), v.getContext().getText(R.string.max_value_must_be_greater_than_min), Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(v.getContext(), v.getContext().getText(R.string.invalid_value), Toast.LENGTH_SHORT).show();
                     }
-                }catch (NumberFormatException e){
-                    Toast.makeText(v.getContext(), v.getContext().getText(R.string.invalid_value), Toast.LENGTH_SHORT).show();
+                } else {
+                    mListener.onSaveYAxisValues();
+                    mAlarmAlertDialog.dismiss();
                 }
                 break;
         }
